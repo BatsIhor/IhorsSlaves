8f41847ed231f5caab869d3b944faac1852757bc
aarsenn <aaarsennn@bigmir.net>
2013-09-16 16:48:41 +0300

    this one too )

---
+++ IhorsSlaves/Models/ViewPostModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IhorsSlaves.Models;
+
+namespace IhorsSlaves.Models
+{
+    public class ViewPostModel
+    {
+        public Post Post { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
