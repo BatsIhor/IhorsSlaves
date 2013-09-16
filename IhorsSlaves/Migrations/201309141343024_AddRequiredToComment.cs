499ddf7d7d4fc6f514276b73d86f684f5e5fb7d1
aarsenn <aaarsennn@bigmir.net>
2013-09-16 16:46:45 +0300

    looks like it's used somehow

---
+++ IhorsSlaves/Migrations/201309141343024_AddRequiredToComment.cs
@@ -0,0 +1,22 @@
+namespace IhorsSlaves.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRequiredToComment : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Comments", "User", c => c.String(nullable: false, maxLength: 40));
+            AlterColumn("dbo.Comments", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Comments", "Content", c => c.String(nullable: false, maxLength: 800));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Comments", "Content", c => c.String(maxLength: 800));
+            AlterColumn("dbo.Comments", "Email", c => c.String());
+            AlterColumn("dbo.Comments", "User", c => c.String(maxLength: 40));
+        }
+    }
+}
