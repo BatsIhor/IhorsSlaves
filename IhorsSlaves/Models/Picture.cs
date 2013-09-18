c1ad5a3bcbea436f1d760c551daf4aad7eb3cc16
aarsenn <aaarsennn@bigmir.net>
2013-09-18 15:11:51 +0300

    Add Picture model

---
+++ IhorsSlaves/Models/Picture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IhorsSlaves.Models
+{
+    [Table("Pictures")]
+    public class Picture
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
+        public int PictureId { get; set; }
+
+        public string Tag { get; set; }
+
+        public string User { get; set; }
+
+        public DateTime UploadDate { get; set; }
+    }
+}
